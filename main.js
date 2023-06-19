e030744ac0b2b1b63b4dc36b73006887c1b7c224
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2023-06-19 17:54:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -30,6 +30,12 @@ ball = {
 
 function setup(){
   canvas =  createCanvas(700,550);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(700, 550);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
 }
 
 
@@ -171,4 +177,9 @@ function paddleInCanvas(){
   if(mouseY < 0){
     mouseY =0;
   }  
+}
+
+function modelLoaded()
+{
+  console.log('Modelo Carregado!');
 }
